85f0f0fa975818ff4dd08794f86bcb923cf57857
Shawn P. Serbin <sserbin@bnl.gov>
2022-03-28 17:32:30 -0400

    Updates

---
+++ scripts/hlm-fates/elm_run_scripts/basic_f19_g16_ICLM45_test_case_e3sm1.0.0.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /E3SM/cime/scripts
+./create_newcase --case /output/f19_g16.ICLM45 --res f19_g16 --compset ICLM45 --mach docker --compiler gnu
+cd /output/f19_g16.ICLM45
+./xmlchange DATM_CLMNCEP_YR_END=1972
+./xmlchange PIO_TYPENAME=netcdf
+./xmlchange RUNDIR=${PWD}/run 
+./xmlchange EXEROOT=${PWD}/bld
+./xmlchange NTASKS=1 
+./xmlchange DIN_LOC_ROOT_CLMFORC=/data/atm/datm7
+./xmlchange DIN_LOC_ROOT=/data/
+cd /output/f19_g16.ICLM45
+./case.setup
+./case.build
