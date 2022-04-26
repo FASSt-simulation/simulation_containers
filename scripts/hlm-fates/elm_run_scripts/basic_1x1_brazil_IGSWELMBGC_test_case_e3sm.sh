b2d3c1fbe249818c27d90e474d3795172605d4fe
Shawn P. Serbin <sserbin@bnl.gov>
2022-04-26 10:21:02 -0400

    Still working out COPY syntax

--- scripts/hlm-fates/elm_run_scripts/basic_1x1_brazil_IGSWELMBGC_test_case_e3sm.sh
+++ scripts/hlm-fates/elm_run_scripts/basic_1x1_brazil_IGSWELMBGC_test_case_e3sm.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 export date_var=$(date +%s)
-export CASE_NAME=/output/1x1_brazil.ICLM45.${date_var}
+export CASE_NAME=/output/1x1_brazil.IGSWELMBGC.${date_var}
 cd /E3SM/cime/scripts
-./create_newcase --case ${CASE_NAME} --res 1x1_brazil --compset ICLM45 --mach docker --compiler gnu
+./create_newcase --case ${CASE_NAME} --res 1x1_brazil --compset IGSWELMBGC --mach docker --compiler gnu
 cd ${CASE_NAME}
 ./xmlchange --id RUN_STARTDATE --val '1980-01-01'
 ./xmlchange --id STOP_OPTION --val nyears
 ./xmlchange --id STOP_N --val 5
 ./xmlchange --id DATM_CLMNCEP_YR_START --val 1980
 ./xmlchange --id DATM_CLMNCEP_YR_END --val 1985
-./xmlchange --id CLM_FORCE_COLDSTART --val on
+./xmlchange --id ELM_FORCE_COLDSTART --val on
 ./xmlchange PIO_TYPENAME=netcdf
 ./xmlchange RUNDIR=${PWD}/run 
 ./xmlchange EXEROOT=${PWD}/bld
 ./xmlchange NTASKS=1
 #./xmlchange NTASKS_LND=4,ROOTPE_LND=0 
-./xmlchange -a CLM_CONFIG_OPTS='-nofire'
+./xmlchange -a ELM_CONFIG_OPTS='-nofire'
 ./xmlchange DIN_LOC_ROOT_CLMFORC=/inputdata/atm/datm7
 ./xmlchange DIN_LOC_ROOT=/inputdata/
 cd ${CASE_NAME}
@@ -26,7 +26,7 @@ echo " "
 ./case.setup
 
 # define output variables and output frequency
-cat >> user_nl_clm <<EOF
+cat >> user_nl_elm <<EOF
 hist_empty_htapes = .true.
 hist_mfilt             = 8760
 hist_nhtfrq            = -1
